fe8446eb5c661c75b82bcddb37874930e18112a9
Hlib <hlib.kalininqa@gmail.com>
2023-05-07 16:46:13 +0300

    Corrected reviewers comments

--- HW2_4.py
+++ HW2_4.py
@@ -1,4 +1,4 @@
-VIP = ["Henrich", "Marcus", "Joseph", "Ulrich"]
+VIP = ["Henrich", "Marcus", "Joseph", "Ulrich", "Irina", "Vladimir"]
 Common = ['Maria', 'Anna', None, 'Max', None, 'Nikolai']
 
 print("VIP Room:", VIP, "Common Room:", Common)
