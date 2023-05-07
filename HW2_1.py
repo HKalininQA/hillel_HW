5b7fa9825fdf96ae6d33b885caa00e9301ca0b42
Hlib <hlib.kalininqa@gmail.com>
2023-05-07 17:44:48 +0300

    fixed variables names

--- HW2_1.py
+++ HW2_1.py
@@ -1,7 +1,7 @@
-Eleks = ['John', 'Mary', 'Anna', 'Max', 'Igor', 'Serhii', 'Oleksandr']
-Toshiba = ['John', 'Max', 'Oksana', 'Sasha', 'Anna', 'Alex', 'Nikolai']
+eleks = ['John', 'Mary', 'Anna', 'Max', 'Igor', 'Serhii', 'Oleksandr']
+toshiba = ['John', 'Max', 'Oksana', 'Sasha', 'Anna', 'Alex', 'Nikolai']
 
-Toshiba.extend(Eleks)
-Eleks.clear()
-print('Toshiba employees:', Toshiba)
-print('Eleks employees:', Eleks)
+toshiba.extend(eleks)
+eleks.clear()
+print('Toshiba employees:', toshiba)
+print('Eleks employees:', eleks)
