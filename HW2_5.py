bc1e828299c7e2f9967e3a4a6ad669ba80297141
Hlib <hlib.kalininqa@gmail.com>
2023-05-07 16:46:22 +0300

    Corrected reviewers comments

--- HW2_5.py
+++ HW2_5.py
@@ -1,10 +1,3 @@
 names = ['John Doe', 'John Dow', 'Mary Sued', 'Mark Sue', 'Max Doe', 'Mary Sued', 'John Doe', 'Bob Hill', 'Bob Hill']
-
-names_unique = []
-
-for name in names:
-    if name not in names_unique:
-        names_unique.append(name)
-
-print(names_unique)
-
+unique_names = list(dict.fromkeys(names))
+print(unique_names)
