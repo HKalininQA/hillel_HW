59888b831afec4852510b635d961a5ccb5110a5b
Hlib <hlib.kalininqa@gmail.com>
2023-05-07 16:46:04 +0300

    Corrected reviewers comments

--- HW2_3.py
+++ HW2_3.py
@@ -1,7 +1,10 @@
 omnivores = ['John', 'Mary', 'Anna', 'Mark']
 vegetarians = ['Josef', 'Julia', 'Hamood', 'Habibi']
-guests = []
+guests= []
 
-omnivores.extend(vegetarians)
-print('List of guests:', omnivores)
+#guests = [*omnivores, *vegetarians]
+
+guests.extend(omnivores)
+guests.extend(vegetarians)
+print('List of guests who can eat vegetables and herbs:', guests)
 
