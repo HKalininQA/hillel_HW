41f4334486ee8e6a9982149f4b4c62916896ce97
Hlib <hlib.kalininqa@gmail.com>
2023-05-10 00:13:10 +0300

    Done second task from HW3

---
+++ HW3_2.py
@@ -0,0 +1,17 @@
+#First approach to multiplying
+a = 2
+result = 2 * 2
+print(f'Result of first multiply:{result}')
+
+#Second approach to multiplying
+a *= 2
+print(f'Result of second multiply:{a}')
+
+#First approach to dividing
+a = 2
+res = 2/2
+print(f'Result of first divide:{res}')
+
+#Second approach to multiplying
+a /= 2
+print(f'Result of second divide:{a}')
