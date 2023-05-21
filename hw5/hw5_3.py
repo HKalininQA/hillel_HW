5ba00ebe776b5b6942c916b3d0722c3f67d89524
Hlib <hlib.kalininqa@gmail.com>
2023-05-21 10:04:30 +0300

    Fixed comments

--- hw5/hw5_3.py
+++ hw5/hw5_3.py
@@ -2,16 +2,15 @@ file_path = '../hw5/text.txt'
 letter_count = {}
 
 with open(file_path, 'r') as text:
-    for line in text:
-        for let in line:
-            if let.isalpha():
-                let = let.lower()
-                if let in letter_count:
-                    letter_count[let] += 1
-                else:
-                    letter_count[let] = 1
+    content = text.read()
+    content = content.lower()
+
+for let in content:
+    if let.isalpha():
+        if let in letter_count:
+            letter_count[let] += 1
+        else:
+            letter_count[let] = 1
 
 for letter, count in letter_count.items():
     print(f"{letter}: {count}")
-
-
